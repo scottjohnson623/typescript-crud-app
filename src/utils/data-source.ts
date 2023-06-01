899a08d3b1fd5bf8a5dcb367d99d6ef7af96b5c9
Scott Johnson <scott.johnson623@gmail.com>
2023-06-01 10:25:14 +0900

    Add utils files, install more npm packages

---
+++ src/utils/data-source.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import config from 'config';
+
+dotenv.config();
+
+const postgresConfig = config.get<{
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+}>('postgresConfig');
+
+export const AppDataSource = new DataSource({
+  ...postgresConfig,
+  type: 'postgres',
+  synchronize: false,
+  logging: false,
+  entities: ['src/entities/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/**/*{.ts,.js}'],
+  subscribers: ['src/subscribers/**/*{.ts,.js}'],
+});
