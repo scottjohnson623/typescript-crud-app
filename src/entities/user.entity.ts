eac1b77451e53f5ad6fee570317b354e0080fdbd
Scott Johnson <scott.johnson623@gmail.com>
2023-06-01 13:29:03 +0900

    Add user entity and migrations, modify tsconfig

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,43 @@
+import crypto from 'crypto';
+import { Entity, Column, Index, BeforeInsert } from 'typeorm';
+import bcrypt from 'bcryptjs';
+import Model from './model.entity';
+
+export enum RoleEnumType {
+  USER = 'user',
+  ADMIN = 'admin',
+}
+
+@Entity('users')
+export class User extends Model {
+  @Column()
+  name: string;
+
+  @Index('email_index')
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+
+  static async comparePasswords(
+    candidatePassword: string,
+    hashedPassword: string
+  ) {
+    return await bcrypt.compare(candidatePassword, hashedPassword);
+  }
+
+  toJSON() {
+    return {
+      ...this,
+      password: undefined,
+    };
+  }
+}
