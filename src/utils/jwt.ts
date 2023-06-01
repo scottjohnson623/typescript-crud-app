c46295ad6241c67b2ed3e02e0856d70b48103534
Scott Johnson <scott.johnson623@gmail.com>
2023-06-01 10:28:39 +0900

    Add db initialization into server initialization

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -4,27 +4,18 @@ import config from 'config';
 export const signJwt = (
   payload: Record<string, unknown>,
   keyName: 'accessTokenPrivateKey' | 'refreshTokenPrivateKey',
-  options: SignOptions
+  options: SignOptions,
 ) => {
-  const privateKey = Buffer.from(
-    config.get<string>(keyName),
-    'base64'
-  ).toString('ascii');
+  const privateKey = Buffer.from(config.get<string>(keyName), 'base64').toString('ascii');
   return jwt.sign(payload, privateKey, {
     ...(options && options),
     algorithm: 'RS256',
   });
 };
 
-export const verifyJwt = <T>(
-  token: string,
-  keyName: 'accessTokenPublicKey' | 'refreshTokenPublicKey'
-): T | null => {
+export const verifyJwt = <T>(token: string, keyName: 'accessTokenPublicKey' | 'refreshTokenPublicKey'): T | null => {
   try {
-    const publicKey = Buffer.from(
-      config.get<string>(keyName),
-      'base64'
-    ).toString('ascii');
+    const publicKey = Buffer.from(config.get<string>(keyName), 'base64').toString('ascii');
     const decoded = jwt.verify(token, publicKey) as T;
 
     return decoded;
